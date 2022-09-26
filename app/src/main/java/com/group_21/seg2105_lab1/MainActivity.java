9708af1d132a14e8f824c067308c1792c9787663
Fufs <rafalfufs.rytwinski@gmail.com>
2022-09-26 17:26:10 -0400

    Empty project created

---
+++ app/src/main/java/com/group_21/seg2105_lab1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.group_21.seg2105_lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
