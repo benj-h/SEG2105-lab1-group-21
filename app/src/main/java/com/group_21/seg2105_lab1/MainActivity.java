607711ed0df5cd59897a54d66638ea1c8dab56d6
benj-h <bheto052@uottawa.ca>
2022-10-10 15:01:18 -0400

    Implemented backspace handling behaviour

--- app/src/main/java/com/group_21/seg2105_lab1/MainActivity.java
+++ app/src/main/java/com/group_21/seg2105_lab1/MainActivity.java
@@ -172,7 +172,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void remove_last_character() {
-        // TODO: Implement remove_last_character()
+        if (values.length() > 0) {
+            values = values.substring(0, values.length() - 1);
+        }
     }
 
     public static State get_state() {
